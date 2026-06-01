5b5d16c344357a96d7c29628da2dd3b4f451c376
Baba01hacker666 <117832562+Baba01hacker666@users.noreply.github.com>
2026-06-01 10:28:52 +0545

    feat(distributed): add reedsolomon dep and update types for sharding

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,14 +1,26 @@
 package types
 
+type ShardInfo struct {
+	Index   int    `json:"index"`
+	ShardID string `json:"shard_id"`
+	NodeID  string `json:"node_id"` // Will be "local" for now
+}
+
+type ChunkInfo struct {
+	Index  int         `json:"index"`
+	Size   int         `json:"size"` // Original encrypted chunk size
+	Shards []ShardInfo `json:"shards"`
+}
+
 type FileRecord struct {
-	ID         string   `json:"id"`
-	Filename   string   `json:"filename"`
-	Size       int64    `json:"size"`
-	MimeType   string   `json:"mime_type"`
-	Compressed bool     `json:"compressed"`
-	Chunks     []string `json:"chunks"`
-	Created    int64    `json:"created"`
-	Modified   int64    `json:"modified"`
+	ID         string      `json:"id"`
+	Filename   string      `json:"filename"`
+	Size       int64       `json:"size"`
+	MimeType   string      `json:"mime_type"`
+	Compressed bool        `json:"compressed"`
+	Chunks     []ChunkInfo `json:"chunks"`
+	Created    int64       `json:"created"`
+	Modified   int64       `json:"modified"`
 }
 
 type StatusReply struct {
